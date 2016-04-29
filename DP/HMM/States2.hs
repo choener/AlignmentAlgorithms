ce8c89c341997553dfb4c1123783c1bc3f33d8ee
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2016-04-30 00:07:42 +0200

    added HMM system

---
+++ DP/HMM/States2.hs
@@ -0,0 +1,27 @@
+
+module DP.HMM.States2 where
+
+import           ADP.Fusion
+import           Data.PrimitiveArray hiding (toList)
+import           FormalLanguage
+
+
+
+[formalLanguage|
+Verbose
+Grammar: HMM
+N: P  -- plus case
+N: M  -- minus case
+T: x
+S: M  -- we should start and stop here
+P -> pstay <<< P x
+P -> mtop  <<< M x
+M -> mstay <<< M x
+M -> ptom  <<< P x
+M -> done  <<< e
+//
+Emit: HMM
+|]
+
+makeAlgebraProduct ''SigHMM
+
