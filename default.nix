3f59e10ef2d036d3c27e029b9cc500ca08954281
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-03-04 00:15:33 +0100

    move to envhs

--- default.nix
+++ default.nix
@@ -1,2 +1,16 @@
-with (import <nixpkgs> {});
-hsDevFunctions ./.
+{ mkDerivation, ADPfusion, base, bytestring, containers, fmlist
+, FormalGrammars, GrammarProducts, PrimitiveArray, stdenv, vector
+}:
+mkDerivation {
+  pname = "AlignmentAlgorithms";
+  version = "0.1.1.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    ADPfusion base bytestring containers fmlist FormalGrammars
+    GrammarProducts PrimitiveArray vector
+  ];
+  testHaskellDepends = [ base ];
+  homepage = "https://github.com/choener/AlignmentAlgorithms";
+  description = "Collection of alignment algorithms";
+  license = stdenv.lib.licenses.gpl3;
+}
